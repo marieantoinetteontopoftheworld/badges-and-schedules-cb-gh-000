0bf35585f7b8c38f8beb9406118f8169b073482d
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 13:08:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def badge_maker(string)
+  puts "Hello, my name is #{string}"
+end
+
+def batch_badge_maker(string_array)
+  string_array.collect{|string| badge_maker(string)}
+end
